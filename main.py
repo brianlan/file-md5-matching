e0672e7132dd3f7e9be30015a5c29788c5246d7d
brianlan <brianlanbo@gmail.com>
2018-11-28 11:16:12 +0800

    refactored a bit and made it to support multiprocessing

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ import shutil
 import argparse
 import pathlib
 import subprocess
-import platform
+import multiprocessing
 
 parser = argparse.ArgumentParser(description='Find the matching between folders of files.')
 parser.add_argument('--dir-queries', required=True, help='Dir of the files to be queried about.')
 parser.add_argument('--dir-file-pool', required=True, help='Dir of all the entire files.')
 parser.add_argument('--result-save-path', required=True)
 parser.add_argument('--suffix')
+parser.add_argument('--num-processes', default=1)
 
 
-def get_md5sum_stdout(dir_path, suffix):
-    exec_str = 'find %s -name "*.%s" | xargs md5sum' % (dir_path, suffix)
-    p = subprocess.Popen(exec_str, stdout=subprocess.PIPE, shell=True)
-    return p.communicate()
+def get_file_md5sum(file_path):
+    p = subprocess.Popen('md5sum %s' % file_path, stdout=subprocess.PIPE, shell=True)
+    output, err = p.communicate()
+    return output.split()[0].decode()
 
 
-def calc_md5_for_dir(dir_path, suffix):
-    output, err = get_md5sum_stdout(dir_path, suffix)
-    lines = [line.split() for line in output.strip().split(b'\n')]
-    return {line[0].decode(): line[1].decode() for line in lines}
+def get_file_paths(root_dir, suffix):
+    p = subprocess.Popen('find %s -name "*.%s"' % (root_dir, suffix), stdout=subprocess.PIPE, shell=True)
+    output, err = p.communicate()
+    return [line.strip().decode() for line in output.strip().split(b'\n')]
+
+
+def calc_md5_for_dir(dir_path, suffix, num_processes=1):
+    print('Calculating md5 for %s files in %s ...' % (suffix, dir_path))
+    file_paths = get_file_paths(dir_path, suffix)
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        md5 = pool.map(get_file_md5sum, file_paths)
+    return {m: p for m, p in zip(md5, file_paths)}
 
 
 def write_results(results, save_path):
@@ -28,29 +37,25 @@ def write_results(results, save_path):
     with open(save_path, 'w') as f:
         for query_path, matching_path in results.items():
             f.write('{},{}\n'.format(query_path, matching_path or ''))
+    print('Matching results has been saved to %s' % save_path)
 
 
 def calc_match_results(queries, file_pool):
+    print('Calculating matching results between the above directories ...')
     return {path: file_pool.get(md5) for md5, path in queries.items()}
 
 
-def check_platform_support():
-    if platform.system().lower() == 'windows':
-        print('Sorry, this tool is not supported on Windows platform yet.')
+def check_linux_command_exists(commands):
+    if any(shutil.which(cmd) is None for cmd in commands):
+        print('Sorry, this tool requires commands: {}.'.format(commands))
         exit(-1)
-
-
-def check_md5sum_exists():
-    if shutil.which('md5sum') is None:
-        print('Sorry, this tool depends on md5sum but can not be found.')
-        exit(-1)
+    print('Passed dependency checking for {}'.format(commands))
 
 
 def main(args):
-    check_platform_support()
-    check_md5sum_exists()
-    queries = calc_md5_for_dir(args.dir_queries, args.suffix)
-    file_pool = calc_md5_for_dir(args.dir_file_pool, args.suffix)
+    check_linux_command_exists(['find', 'xargs', 'md5sum'])
+    queries = calc_md5_for_dir(args.dir_queries, args.suffix, num_processes=args.num_processes)
+    file_pool = calc_md5_for_dir(args.dir_file_pool, args.suffix, num_processes=args.num_processes)
     results = calc_match_results(queries, file_pool)
     write_results(results, args.result_save_path)
 
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+import shutil
 from pathlib import Path as P
 from collections import Iterable
 
-from main import calc_md5_for_dir
+import pytest
+
+from main import calc_md5_for_dir, get_file_paths, get_file_md5sum
 
 
 def construct_test_files(base_dir, contents, suffix=None):
@@ -14,10 +17,45 @@ def construct_test_files(base_dir, contents, suffix=None):
             f.write(content)
 
 
-def test_calc_md5_for_dir():
-    suffix = 'txt'
-    construct_test_files('/tmp/a', ['123456', '123'], suffix=suffix)
-    construct_test_files('/tmp/a/b', ['aaa'], suffix=suffix)
-    assert calc_md5_for_dir('/tmp/a', suffix) == {'47bce5c74f589f4867dbd57e9ca9f808': '/tmp/a/b/aaa.txt',
-                                                  'e10adc3949ba59abbe56e057f20f883e': '/tmp/a/123456.txt',
-                                                  '202cb962ac59075b964b07152d234b70': '/tmp/a/123.txt'}
+@pytest.fixture(scope='module')
+def files_in_tmp():
+    construct_test_files('/tmp/a', ['123456', '123'], suffix='txt')
+    construct_test_files('/tmp/a', ['123456', '123'], suffix='xml')
+    construct_test_files('/tmp/a/b', ['aaa'], suffix='txt')
+    yield
+    shutil.rmtree('/tmp/a')
+
+
+@pytest.fixture(scope='module')
+def files_in_cur_folder():
+    construct_test_files('test_folder_a', ['123456', '123'], suffix='txt')
+    construct_test_files('test_folder_a', ['123456', '123'], suffix='xml')
+    construct_test_files('test_folder_a/test_folder_b', ['aaa'], suffix='txt')
+    yield
+    shutil.rmtree('test_folder_a')
+
+
+def test_calc_md5_for_dir(files_in_tmp):
+    assert calc_md5_for_dir('/tmp/a', 'txt') == {'47bce5c74f589f4867dbd57e9ca9f808': '/tmp/a/b/aaa.txt',
+                                                 'e10adc3949ba59abbe56e057f20f883e': '/tmp/a/123456.txt',
+                                                 '202cb962ac59075b964b07152d234b70': '/tmp/a/123.txt'}
+
+
+def test_get_file_paths(files_in_tmp):
+    assert set(get_file_paths('/tmp/a', 'txt')) == {'/tmp/a/123456.txt', '/tmp/a/123.txt', '/tmp/a/b/aaa.txt'}
+
+
+def test_get_file_paths_given_relpath_root_dir(files_in_cur_folder):
+    assert set(get_file_paths('test_folder_a', 'txt')) == {'test_folder_a/123456.txt', 'test_folder_a/123.txt',
+                                                           'test_folder_a/test_folder_b/aaa.txt'}
+    assert set(get_file_paths('./test_folder_a', 'txt')) == {'./test_folder_a/123456.txt', './test_folder_a/123.txt',
+                                                             './test_folder_a/test_folder_b/aaa.txt'}
+
+
+def test_get_file_md5sum(files_in_tmp):
+    assert get_file_md5sum('/tmp/a/b/aaa.txt') == '47bce5c74f589f4867dbd57e9ca9f808'
+
+
+def test_get_file_md5sum_relpath(files_in_cur_folder):
+    assert get_file_md5sum('test_folder_a/test_folder_b/aaa.txt') == '47bce5c74f589f4867dbd57e9ca9f808'
+    assert get_file_md5sum('./test_folder_a/test_folder_b/aaa.txt') == '47bce5c74f589f4867dbd57e9ca9f808'
